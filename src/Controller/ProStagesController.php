480ce129220b4474d1ae8d45eaa447683987784b
Quentin Marque <qmarque@iutbayonne.univ-pau.fr>
2019-11-21 10:23:10 +0100

    Installation des controlleurs

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -4,16 +4,69 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/pro/stages", name="pro_stages")
+     * @Route("/", name="home")
      */
-    public function index()
+    public function indexHome()
     {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'ProStagesController',
-        ]);
+return $this -> render('pro_stages/indexHome.html.twig');
+
+        /*return new Response('<html><body><h1> Bienvenue sur la page d accueil de Prostages</h1></body></html>');*/
+        
     }
+
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+
+    
+    public function indexEntreprises()
+    {
+        return $this -> render('pro_stages/indexEntreprises.html.twig');
+
+
+        /*return new Response('<html><body><h1> Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');*/
+    
+    }
+
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function indexFormations()
+    {
+        return $this -> render('pro_stages/indexFormations.html.twig');
+
+
+       /* return new Response('<html><body><h1> Cette page affichera la liste des formations de l IUT</h1></body></html>');*/
+    
+    }
+
+
+    /**
+     * @Route("/stages/{id}", name="stages")
+     */
+    public function indexStages($id)
+    {
+
+        return $this -> render('pro_stages/indexStages.html.twig', ['id'=>$id]);
+
+
+       /* return new Response('<html><body><h1> Cette page affichera le descriptif du stage ayant pour identifiant id </h1></body></html>');*/
+    
+    }
+
+    
+
+    /*public functon index()
+    {
+        return $this->render-'pro_stages/index.html.twig', [
+            'controller_name' => 'ProStagesController',]);
+        ]
+    }*/
 }
