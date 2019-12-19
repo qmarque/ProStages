18c879354ede3ba32d8313966f3b2625d5337c5c
Quentin Marque <qmarque@iutbayonne.univ-pau.fr>
2019-12-19 14:59:52 +0100

    Insertion des stages

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,122 +14,60 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $formationDUTInfo = new Formation();
-        $formationDUTInfo->setNomLong("Diplôme universitaire et Technologique Informatique");
-        $formationDUTInfo->setNomCourt("DUT Info");
-        $manager->persist($formationDUTInfo);
-
-        $formationLPMul = new Formation();
-        $formationLPMul->setNomLong("Licence professionnelle multimédia");
-        $formationLPMul->setNomCourt("LP Multimédia");
-        $manager->persist($formationLPMul);
-
-        $formationDUTIC = new Formation();
-        $formationDUTIC->setNomLong("Diplôme universitaire en Technologies de l'Information et de la Communication");
-        $formationDUTIC->setNomCourt("DU TIC");
-        $manager->persist($formationDUTIC);
-
         //Création d'un générateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
 
-        //Génaration de données de test pour les Entreprises dans une boucle for
-       /* $nbEntreprises=10;
-        for($i=0;$i<=$nbEntreprises;$i++){
-            $Entreprise = new Entreprise();
-            $Entreprise->setNom($faker->regexify('[A-Z][a-z]{5,12}'));
-            $Entreprise->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-            $Entreprise->setAdresse($faker->address);
-            $Entreprise->setSiteWeb($faker->url);
-            $manager->persist($Entreprise);
-        }
-    */
-
-        //Génaration de données de test pour les Entreprises excepté pour le nom de l'entreprise
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("Capgemini");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("Cybertek");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("Digiway");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("SEI GROUPE LKS");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("ALGO'TECH INFORMATIQUE");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("BIGWIKI");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("IM DIGITAL");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("WEETAB");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("SITINCLOUD");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("CPAM");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("SARL HARADA");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
-
-        $Entreprise1 = new Entreprise();
-        $Entreprise1->setNom("SMILE AND GOLF");
-        $Entreprise1->setActivite($faker->realText($maxNbChars=150,$indexSize=2));
-        $Entreprise1->setAdresse($faker->address);
-        $Entreprise1->setSiteWeb($faker->url);
-        $manager->persist($Entreprise1);
 
         
+
+        //Génaration de données de test (excepté les noms) pour les entreprises 
+        $nbEntreprises = 30; //$faker->numberBetween($min = 1,$max = 10);
+        for ($i=0; $i < $nbEntreprises; $i++) { 
+            $uneEntreprise[$i] = new Entreprise();
+            $uneEntreprise[$i]->setNom($faker->regexify('(Capgemini|Cybertek|Digiway|SEI GROUPE LKS|ALGO TECH INFORMATIQUE|BIGWIKI|IM DIGITAL|WEETAB|SITINCLOUD|CPAM|SARL HARADA|SMILE AND GOLF)'));
+            $uneEntreprise[$i]->setActivite($faker->catchPhrase);
+            $uneEntreprise[$i]->setAdresse($faker->address);
+            $uneEntreprise[$i]->setSiteWeb($faker->url);
+            $manager->persist($uneEntreprise[$i]);
+        }
+
+       
+        //Génaration de données de test pour les formations 
+        $tableauFormations = array("DUT Info"=>"DUT Informatique","LPM"=>"Licence Pro Multimédia","DUTIC"=>"DU Technologie de l'Information");
+        foreach ($tableauFormations as $nomCourt => $nomLong) {
+            $uneFormation = new Formation();
+            $uneFormation->setNomCourt($nomCourt);
+            $uneFormation->setNomLong($nomLong);
+            $manager->persist($uneFormation); 
+        
+                
+
+
+        
+       //Création de plusieurs stages associés aux formations
+        $nbStages = 10;
+        for ($i=0; $i < $nbStages; $i++) {
+            $stage = new Stage();
+            $stage -> setTitre($faker->jobTitle);
+            $stage -> setDescription($faker->realText($maxNbChars = 400, $indexSize = 2));
+            $stage -> setEmail($faker->email);
+            // Création relation Stage --> Formation
+            $stage -> addFormation($uneFormation);
+            
+
+            //Définir et mettre à jour l'entreprise
+            // Sélectionner une entreprise au hasard parmi les 12 enregistrées dans $tableauEntreprises
+            $numEntreprises = $faker->numberBetween($min = 0,$max = 11);
+            // Création relation Stage --> Entreprise
+            $stage -> setEntreprise($uneEntreprise[$numEntreprises]);
+            // Création relation Entreprise --> Stage
+            $uneEntreprise[$numEntreprises] -> addStage($stage);
+            // Persister les objets modifiés
+            $manager->persist($stage);
+            $manager->persist($uneEntreprise[$numEntreprises]);
+        }
+    } 
+        //Envoi les objets crées en base de données
         $manager->flush();
     }
 }
